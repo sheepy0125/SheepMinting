4e07483ea6cbfe908a3fcdee42c3f73293fe0810
sheepy0125 <sheepy404@gmail.com>
2022-01-29 14:35:10 -0500

    Add contract

--- web3app/test/sample-test.js
+++ web3app/test/sample-test.js
@@ -1,19 +1,27 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const {expect} = require("chai");
+const {ethers} = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("SheepToken", function () {
+	it("Should mint and transfer an NFT to someone", async function () {
+		const SheepToken = await ethers.getContractFactory("SheepToken");
+		const sheepToken = await SheepToken.deploy();
+		await sheepToken.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+		const recipient = "0x9965507d1a55bcc2695c58ba16fb37d819b0a4dc";
+		const metadataURI = "cid/test.png";
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+		let balance = await sheepToken.balanceOf(recipient);
+		expect(balance).to.equal(0);
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+		const newMintedToken = await sheepToken.payToMint(recipient, metadataURI, {
+			value: ethers.utils.parseEther("0.05"),
+		});
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+		await newMintedToken.wait();
+
+		balance = await sheepToken.balanceOf(recipient);
+		expect(balance).to.equal(1);
+
+		expect(await sheepToken.isContentOwned(metadataURI)).to.equal(true);
+	});
 });
